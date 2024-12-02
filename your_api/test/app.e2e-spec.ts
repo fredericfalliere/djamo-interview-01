0ef0243955fdf6413ff9eb4fcdb366b1d89dd659
Frederic Falliere <frederic.falliere@gmail.com>
2024-12-02 11:13:29 +0100

    Uncommenting older tests : it is still green, yay

--- your_api/test/app.e2e-spec.ts
+++ your_api/test/app.e2e-spec.ts
@@ -55,84 +55,84 @@ describe('Third party API', () => {
       });
   }, 45_000);
 
-  // it('should be working in perfect conditions aka the nominal case', async () => {
-  //   let transactionId: number;
-  //   const workingConditions = {
-  //     shouldTimeout: false,
-  //     shouldSendWebhook: false,
-  //   }
+  it('should be working in perfect conditions aka the nominal case', async () => {
+    let transactionId: number;
+    const workingConditions = {
+      shouldTimeout: false,
+      shouldSendWebhook: false,
+    }
 
-  //   await request(app.getHttpServer())
-  //     .post('/transaction')
-  //     .send({ amount: 20, workingConditions })
-  //     .expect((res) => {
-  //       transactionId = res.body.id;
-  //     })
-  //     .then(async () => {
+    await request(app.getHttpServer())
+      .post('/transaction')
+      .send({ amount: 20, workingConditions })
+      .expect((res) => {
+        transactionId = res.body.id;
+      })
+      .then(async () => {
         
-  //       expect(await getTransaction(transactionId)).toEqual(TransactionStatus.initiated);
+        expect(await getTransaction(transactionId)).toEqual(TransactionStatus.initiated);
         
-  //       await new Promise<void>(resolve => setTimeout(async () => {
-  //         const transaction = await transactionService.findById(transactionId);
-  //         expect(transaction).not.toBeNull();
-  //         if (transaction != null) {
-  //           expect(transaction.status == TransactionStatus.success ||
-  //             transaction.status == TransactionStatus.failure).toBe(true);
-  //         }
-  //         resolve();
-  //       }, 10000));
+        await new Promise<void>(resolve => setTimeout(async () => {
+          const transaction = await transactionService.findById(transactionId);
+          expect(transaction).not.toBeNull();
+          if (transaction != null) {
+            expect(transaction.status == TransactionStatus.success ||
+              transaction.status == TransactionStatus.declined).toBe(true);
+          }
+          resolve();
+        }, 10000));
         
 
-  //     });
-  // },  15000);
+      });
+  },  15000);
 });
 
-// describe('HTTP POST Transaction on our backend', () => {
+describe('HTTP POST Transaction on our backend', () => {
 
-//   it('should return an initiated transaction with an id, and fast, and add a transactions to the DB', async () => {
-//     const startTime = performance.now();
-//     const startTransactions = await transactionService.countAll();
-//     let transactionId:number;
+  it('should return an initiated transaction with an id, and fast, and add a transactions to the DB', async () => {
+    const startTime = performance.now();
+    const startTransactions = await transactionService.countAll();
+    let transactionId:number;
 
-//     await request(app.getHttpServer())
-//       .post('/transaction')
-//       .send({ amount: 15 })
-//       .expect(201)
-//       .expect((res) => {
-//         expect(res.body.amount).toEqual(15)
-//         expect(res.body.status).toEqual(1) 
-//         expect(res.body.id).not.toBeNull();
-//         transactionId = res.body.id;
-//         const time = performance.now() - startTime;
-//         expect(time).toBeLessThan(400);
+    await request(app.getHttpServer())
+      .post('/transaction')
+      .send({ amount: 15 })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.amount).toEqual(15)
+        expect(res.body.status).toEqual(1) 
+        expect(res.body.id).not.toBeNull();
+        transactionId = res.body.id;
+        const time = performance.now() - startTime;
+        expect(time).toBeLessThan(400);
 
-//       })
-//       .then(async () => {
-//         const endTransactions = await transactionService.countAll();
-//         expect(endTransactions).toEqual(startTransactions + 1);
+      })
+      .then(async () => {
+        const endTransactions = await transactionService.countAll();
+        expect(endTransactions).toEqual(startTransactions + 1);
 
-//         const transaction = await transactionService.findById(transactionId);
-//         expect(transaction).not.toBeNull();
-//         if (transaction != null) {
-//           expect(transaction.amount).toEqual(15);
-//         }
-//     })
-//   });
+        const transaction = await transactionService.findById(transactionId);
+        expect(transaction).not.toBeNull();
+        if (transaction != null) {
+          expect(transaction.amount).toEqual(15);
+        }
+    })
+  });
 
-//   it('should fail if amount is a string', () => {
-//     return request(app.getHttpServer())
-//       .post('/transaction')
-//       .send({ amount: 'ahah' })
-//       .expect(400)
-//   });
+  it('should fail if amount is a string', () => {
+    return request(app.getHttpServer())
+      .post('/transaction')
+      .send({ amount: 'ahah' })
+      .expect(400)
+  });
 
-//   it('should fail if amount is incorrect', () => {
-//     return request(app.getHttpServer())
-//       .post('/transaction')
-//       .send({ amount: -2 })
-//       .expect(400)
-//   });
-// });
+  it('should fail if amount is incorrect', () => {
+    return request(app.getHttpServer())
+      .post('/transaction')
+      .send({ amount: -2 })
+      .expect(400)
+  });
+});
 
 async function getTransaction(transactionId: number): Promise<number> {
 
